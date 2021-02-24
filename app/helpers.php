ed1b845d9b3370d3da46338deed368d98e5a4361
vagrant <vagrant@homestead>
2021-02-24 08:21:23 +0000

    初始化分期付款模型

--- app/helpers.php
+++ app/helpers.php
@@ -10,6 +10,12 @@
         return route($routeName, $parameters);
     }
 
+    // 默认的精度为小数点后两位
+    function big_number($number, $scale = 2)
+    {
+        return new \Moontoast\Math\BigNumber($number, $scale);
+    }
+
 
 
 ?>
