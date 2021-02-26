9c3788d510c1429e6cb75bdf3bc4214ea846a7b2
vagrant <vagrant@homestead>
2021-02-26 09:25:55 +0000

    分期付款退款

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use App\Models\Order;
 use App\Events\OrderPaid;
 use Endroid\QrCode\QrCode;
 use App\Models\Installment;
 use Illuminate\Http\Request;
+use App\Models\InstallmentItem;
 use App\Exceptions\InvalidRequestException;
 
 class InstallmentsController extends Controller
@@ -71,63 +73,63 @@ class InstallmentsController extends Controller
         return view('pages.success', ['msg' => '付款成功']);
     }
 
-    // 支付宝后端回调
-    // public function alipayNotify()
-    // {
-    //     // 校验支付宝回调参数是否正确
-    //     $data = app('alipay')->verify();
-    //     // 如果订单状态不是成功或者结束，则不走后续的逻辑
-    //     if (!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
-    //         return app('alipay')->success();
-    //     }
-    //     // 拉起支付时使用的支付订单号是由分期流水号 + 还款计划编号组成的
-    //     // 因此可以通过支付订单号来还原出这笔还款是哪个分期付款的哪个还款计划
-    //     list($no, $sequence) = explode('_', $data->out_trade_no);
-    //     // 根据分期流水号查询对应的分期记录，原则上不会找不到，这里的判断只是增强代码健壮性
-    //     if (!$installment = Installment::where('no', $no)->first()) {
-    //         return 'fail';
-    //     }
-    //     // 根据还款计划编号查询对应的还款计划，原则上不会找不到，这里的判断只是增强代码健壮性
-    //     if (!$item = $installment->items()->where('sequence', $sequence)->first()) {
-    //         return 'fail';
-    //     }
-    //     // 如果这个还款计划的支付状态是已支付，则告知支付宝此订单已完成，并不再执行后续逻辑
-    //     if ($item->paid_at) {
-    //         return app('alipay')->success();
-    //     }
+    /*// 支付宝后端回调
+    public function alipayNotify()
+    {
+        // 校验支付宝回调参数是否正确
+        $data = app('alipay')->verify();
+        // 如果订单状态不是成功或者结束，则不走后续的逻辑
+        if (!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
+            return app('alipay')->success();
+        }
+        // 拉起支付时使用的支付订单号是由分期流水号 + 还款计划编号组成的
+        // 因此可以通过支付订单号来还原出这笔还款是哪个分期付款的哪个还款计划
+        list($no, $sequence) = explode('_', $data->out_trade_no);
+        // 根据分期流水号查询对应的分期记录，原则上不会找不到，这里的判断只是增强代码健壮性
+        if (!$installment = Installment::where('no', $no)->first()) {
+            return 'fail';
+        }
+        // 根据还款计划编号查询对应的还款计划，原则上不会找不到，这里的判断只是增强代码健壮性
+        if (!$item = $installment->items()->where('sequence', $sequence)->first()) {
+            return 'fail';
+        }
+        // 如果这个还款计划的支付状态是已支付，则告知支付宝此订单已完成，并不再执行后续逻辑
+        if ($item->paid_at) {
+            return app('alipay')->success();
+        }
 
-    //     // 使用事务，保证数据一致性
-    //     \DB::transaction(function () use ($data, $no, $installment, $item) {
-    //         // 更新对应的还款计划
-    //         $item->update([
-    //             'paid_at'        => Carbon::now(), // 支付时间
-    //             'payment_method' => 'alipay', // 支付方式
-    //             'payment_no'     => $data->trade_no, // 支付宝订单号
-    //         ]);
+        // 使用事务，保证数据一致性
+        \DB::transaction(function () use ($data, $no, $installment, $item) {
+            // 更新对应的还款计划
+            $item->update([
+                'paid_at'        => Carbon::now(), // 支付时间
+                'payment_method' => 'alipay', // 支付方式
+                'payment_no'     => $data->trade_no, // 支付宝订单号
+            ]);
 
-    //         // 如果这是第一笔还款
-    //         if ($item->sequence === 0) {
-    //             // 将分期付款的状态改为还款中
-    //             $installment->update(['status' => Installment::STATUS_REPAYING]);
-    //             // 将分期付款对应的商品订单状态改为已支付
-    //             $installment->order->update([
-    //                 'paid_at'        => Carbon::now(),
-    //                 'payment_method' => 'installment', // 支付方式为分期付款
-    //                 'payment_no'     => $no, // 支付订单号为分期付款的流水号
-    //             ]);
-    //             // 触发商品订单已支付的事件
-    //             event(new OrderPaid($installment->order));
-    //         }
+            // 如果这是第一笔还款
+            if ($item->sequence === 0) {
+                // 将分期付款的状态改为还款中
+                $installment->update(['status' => Installment::STATUS_REPAYING]);
+                // 将分期付款对应的商品订单状态改为已支付
+                $installment->order->update([
+                    'paid_at'        => Carbon::now(),
+                    'payment_method' => 'installment', // 支付方式为分期付款
+                    'payment_no'     => $no, // 支付订单号为分期付款的流水号
+                ]);
+                // 触发商品订单已支付的事件
+                event(new OrderPaid($installment->order));
+            }
 
-    //         // 如果这是最后一笔还款
-    //         if ($item->sequence === $installment->count - 1) {
-    //             // 将分期付款状态改为已结清
-    //             $installment->update(['status' => Installment::STATUS_FINISHED]);
-    //         }
-    //     });
+            // 如果这是最后一笔还款
+            if ($item->sequence === $installment->count - 1) {
+                // 将分期付款状态改为已结清
+                $installment->update(['status' => Installment::STATUS_FINISHED]);
+            }
+        });
 
-    //     return app('alipay')->success();
-    // }
+        return app('alipay')->success();
+    }*/
 
     public function payByWechat(Installment $installment)
     {
@@ -176,6 +178,46 @@ class InstallmentsController extends Controller
         }
 
         return 'fail';
+    }
+
+    public function wechatRefundNotify(Request $request)
+    {
+        // 给微信的失败响应
+        $failXml = '<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>';
+        // 校验微信回调参数
+        $data = app('wechat_pay')->verify(null, true);
+        // 根据单号拆解出对应的商品退款单号及对应的还款计划序号
+        list($no, $sequence) = explode('_', $data['out_refund_no']);
+
+        $item = InstallmentItem::query()
+            ->whereHas('installment', function ($query) use ($no) {
+                $query->whereHas('order', function ($query) use ($no) {
+                    $query->where('refund_no', $no); // 根据订单表的退款流水号找到对应还款计划
+                });
+            })
+            ->where('sequence', $sequence)
+            ->first();
+
+        // 没有找到对应的订单，原则上不可能发生，保证代码健壮性
+        if (!$item) {
+            return $failXml;
+        }
+
+        // 如果退款成功
+        if ($data['refund_status'] === 'SUCCESS') {
+            // 将还款计划退款状态改成退款成功
+            $item->update([
+                'refund_status' => InstallmentItem::REFUND_STATUS_SUCCESS,
+            ]);
+            $item->installment->refreshRefundStatus();
+        } else {
+            // 否则将对应还款计划的退款状态改为退款失败
+            $item->update([
+                'refund_status' => InstallmentItem::REFUND_STATUS_FAILED,
+            ]);
+        }
+
+        return app('wechat_pay')->success();
     }
 
     protected function paid($outTradeNo, $paymentMethod, $paymentNo)
