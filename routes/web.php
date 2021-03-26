e3e6ac2e7809c5377717d278cc431a9cf4314d8f
qing zhang <mn19930301@126.com>
2021-03-27 01:02:22 +0800

    更改zhifubao

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
-Route::get('alipay', function() {
-    return app('alipay')->web([
-        'out_trade_no' => time(),
-        'total_amount' => '1',
-        'subject' => 'test subject - 测试',
-    ]);
-});
+// Route::get('alipay', function() {
+//     return app('alipay')->web([
+//         'out_trade_no' => time(),
+//         'total_amount' => '1',
+//         'subject' => 'test subject - 测试',
+//     ]);
+// });
 
 Auth::routes(['verify' => true]);
 
